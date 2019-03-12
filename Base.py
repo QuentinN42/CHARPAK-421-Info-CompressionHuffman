ae82e783fd172512cfdcdac45d31b328a52b0f2e
Number42 <quentin.lieumont@gmail.com>
2019-03-12 17:29:04 +0100

    utilisation de pytest et ajout d'un fichier données utiles

--- Base.py
+++ Base.py
@@ -48,6 +48,9 @@ class Huffman:
         self.foret = []
 
 
+#Fonctions
+
+
 def encode(c: str) -> str:
     """
     code un caractere en ascii 8 bit
@@ -91,6 +94,57 @@ def frequences(txt: str) -> dict:
         else:
             d[c] = 1
     return d
+
+
+#Tests pytest
+
+
+def decorated_test(func):
+    def inner(*args, **kwargs):
+        before = "\n"*3+"="*10
+        print(before, func.__name__, before[::-1])
+        res = func(*args, **kwargs)
+        return res
+    return inner
+
+
+def _test_dict(dico,func) -> list:
+    """
+    test une fonction pour chaque element d'un dictionnaire
+    
+    :param dico: dictionnaire input, expected output
+    :param func: fonction a tester
+    :return: tableau des resultats des tests
+    """
+    tab = []
+    for key in dico.keys():
+        print('Input :', key)
+        expected = dico[key]
+        result = func(key)
+        print('Expected output :', expected)
+        print('Returned output :', result)
+        tab.append(expected == result)
+    return tab
+
+
+@decorated_test
+def test_encode():
+    """
+    test toutes les lettres de l'alphabet contenues dans le fichier usefull.py
+    """
+    from usefull import alphabet
+    for test in _test_dict(alphabet, encode):
+        assert test
+
+
+@decorated_test
+def test_frequences():
+    """
+    test les combinaisons de str/frequences presentes dans usefull.py
+    """
+    from usefull import freqTest
+    for test in _test_dict(freqTest, frequences):
+        assert test
 
 
 if __name__ == "__main__":
