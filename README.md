15365c1f56f1a7c87106738adc1f7c84798a30e8
QuentinN42 <32516498+quentinn42@users.noreply.github.com>
2019-03-18 16:57:58 +0100

    Parseur addapté au projet

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ DM S4 Radiofrequence \
 Institut Villebon Georges Charpak
 
 #### Utilisation de codes extenres :
-Inversion de dictionnaire : [Stack overflow : 483666](https://stackoverflow.com/questions/483666/python-reverse-invert-a-mapping)
+Inversion de dictionnaire : [Stack overflow : 483666](https://stackoverflow.com/questions/483666/python-reverse-invert-a-mapping) \
 Parseur : [Stack overflow : 28479543](https://stackoverflow.com/questions/28479543/run-python-script-with-some-of-the-argument-that-are-optional)
--- test_parsearg.py
+++ test_parsearg.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 # coding: utf-8
 
+"""
+Test du parseur d'arguments
+"""
+
+__version__ = '1.1'
+
 import argparse
 
 
@@ -9,17 +15,16 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
 
     # Positional mandatory arguments
-    parser.add_argument("creditMom", help="Credit mom.", type=float)
-    parser.add_argument("creditDad", help="Credit dad.", type=float)
-    parser.add_argument("debtMom", help="Debt mom.", type=float)
+    parser.add_argument("Mode", help="Mode codage (C) ou decodage (D).", type=str)
+    parser.add_argument("InputFile", help="Le fichier a coder ou decoder.", type=str)
 
     # Optional arguments
-    parser.add_argument("-dD", "--debtDad", help="Debt dad.", type=float, default=1000.)
-    parser.add_argument("-s", "--salary", help="Debt dad.", type=float, default=2000.)
-    parser.add_argument("-b", "--bonus", help="Debt dad.", type=float, default=0.)
+    parser.add_argument("-c", "--config", help="Fichier de configuration des frequences", type=str)
+    parser.add_argument("-o", "--Output", help="Fichier de sortie (ecrase)", type=str)
+    parser.add_argument("-ao", "--AppendOutput", help="Fichier de sortie (ecrit a la suite)", type=str)
 
     # Print version
-    parser.add_argument("--version", action="version", version='%(prog)s - Version 1.0')
+    parser.add_argument("--version", action="version", version=__file__ + ' - Version ' + __version__)
 
     # Parse arguments
     args = parser.parse_args()
@@ -28,4 +33,4 @@ def parse_arguments():
 
 
 if __name__ == "__main__":
-    print(parse_arguments())
+    print(parse_arguments().InputFile)
